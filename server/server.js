3df107b8197e04909639139ad01fa91f6dd0948f
Chandrasekhar Srinivasa <chasrini@publicisgroupe.net>
2017-12-03 22:40:05 +0530

    port number changed and start command in package.json

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 var app = express();
 var server = http.createServer(app);
